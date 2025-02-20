3cdfd51a31aed12737d56418e30658fba175a262
taylorjeandev <taylor@taylorgjean.com>
2025-02-20 10:42:22 +0100

    az-900 cert

--- my-website/components/Certifications.js
+++ my-website/components/Certifications.js
@@ -7,12 +7,12 @@ export default function Certifications() {
       <div className="mt-6 max-w-3xl mx-auto text-gray-400 space-y-6">
         
         {/* AZ-900 Certification (Replace # with Credly link when available) */}
-        <a href="#" target="_blank" rel="noopener noreferrer" className="block">
+        <a href="https://learn.microsoft.com/en-us/users/taylorgjean/credentials/f60f1c8d6d3104ca?ref=https%3A%2F%2Fwww.linkedin.com%2F" target="_blank" rel="noopener noreferrer" className="block">
           <div className="flex items-center bg-gray-900 p-6 rounded-lg shadow-md hover:bg-gray-800 transition">
             <img src="/az-900-2.jpg" alt="AZ-900 Logo" className="w-16 h-16 mr-4" />
             <div className="text-left">
               <h3 className="text-xl font-semibold text-white">Microsoft Certified: Azure Fundamentals (AZ-900)</h3>
-              <span>In Progress</span>
+              <span>Issued: 2025</span>
               <p>Validates foundational knowledge of Azure cloud services and principles.</p>
             </div>
           </div>
