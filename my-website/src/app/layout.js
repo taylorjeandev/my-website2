b2666f1948782d5e748cffb93815bd0c4a766529
Taylor Jean <taylorgjean@gmail.com>
2026-04-02 14:12:44 +0200

    Removed Experience Section

--- my-website/src/app/layout.js
+++ my-website/src/app/layout.js
@@ -1,14 +1,19 @@
-import "./globals.css";
+// app/layout.js
+import "/styles/globals.css";
+import Navbar from "/components/Navbar";
 
 export const metadata = {
-  title: "My Website",
-  description: "My portfolio",
+  title: "Taylor Jean | Portfolio",
+  description: "IT Specialist | Networking | Content Creator",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="pt-20 min-h-screen bg-gray-100 text-gray-900">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- my-website/src/app/page.js
+++ my-website/src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "../../components/Hero";
 import About from "../../components/About";
 import Skills from "../../components/Skills";
 import Projects from "../../components/Projects";
-import Experience from "../../components/Experience";
+// import Experience from "../../components/Experience";
 import Education from "../../components/Education";
 import Certifications from "../../components/Certifications";
 // import Blog from "../../components/Blog";
@@ -17,7 +17,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      <Experience />
+      {/* <Experience /> */}
       <Education />
       <Certifications />
       {/* <Blog /> */}
